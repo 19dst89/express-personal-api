735cc4f2388c369b75f1fc830dc25d9baee1b2b4
Daniel Thompson <19dst89@gmail.com>
2017-02-10 15:36:04 -0800

    json endpoints with profile hardcoded data

--- server.js
+++ server.js
@@ -46,14 +46,54 @@ app.get('/api', function apiIndex(req, res) {
   // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
   // It would be seriously overkill to save any of this to your database.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    woopsIForgotToDocumentAllMyEndpoints: false, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/19dst89/express_self_api/README.md", // CHANGE ME
+    baseUrl: "https://murmuring-forest-24449.herokuapp.com/", // CHANGE ME
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      // Profile Endpoints
+      {
+        method: "GET",
+        path: "/api",
+        description: "Describes all available endpoints"
+      },
+      {
+        method: "GET",
+        path: "/api/profile",
+        description: "Data about me", // Info about Daniel
+        name: "Daniel Thompson",
+        githubLink: "https://github.com/19dst89",
+        githubProfileImage: "https://avatars2.githubusercontent.com/u/19444505?v=3&u=9f88aee27b50439d42594fff366e079fe8939113&s=400",
+        personalSiteLink: "www.google.com",
+        currentCity: "San Francisco",
+        pets: [{name: "Lynx", type: "Cat"}, {name: "Ziggy", type: "Cat"}, {name: "Marley", type: "Dog", breed: "Labrador"}, {name: "Bailey", type: "Dog", breed: "Labrador"}]
+      },
+      // Places endpoints
+      {
+        method: "GET",
+        path: "/api/places",
+        description: "All the places I have lived"
+      },
+      {
+        method: "GET",
+        path: "/api/places/:id",
+        description: "Get information about a certain place I lived"
+      },
+      {
+        method: "POST",
+        path: "/api/places",
+        description: "Add a new place"
+      },
+      {
+        method: "PUT",
+        path: "/api/places/:id",
+        description: "Edit a place"
+      },
+      {
+        method: "DELETE",
+        path: "/api/places/:id",
+        description: "Remove a place"
+      }
     ]
   })
 });
@@ -66,3 +106,19 @@ app.get('/api', function apiIndex(req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is up and running on http://localhost:3000/');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
