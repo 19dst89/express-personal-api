0edc2bafcaeb0c2a39ce94f96e4adfdd34a493cc
Daniel Thompson <19dst89@gmail.com>
2017-02-12 23:24:11 -0800

    added ajax for profile info and places

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,25 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  // display all places
+  $("#places").click(function(){
+    $("div.profile").remove();
+    // console.log($('.places-wrapper').length);
+    $.ajax({
+      method: 'GET',
+      url: '/api/places',
+      data: 'json',
+      success: onSuccessPlaces,
+      error: onErrorPlaces
+    })
 
-  $.ajax({
-    method: 'GET',
-    url: '/api/places',
-    data: 'json',
-    success: onSuccess,
-        // function(json){
-      // $(".places_wrapper").append(json);
-      // console.log(json[0].city);
-    // },
-    error: onError
-  })
+    // ($('#nextractor').length < 0)
 
-    function onSuccess(json){
-      json.forEach(function(place){
+
+    function onSuccessPlaces(placesArray){
+      placesArray.forEach(function(place){
         $(".places-wrapper").append(`
+          <div class="places">
           <ul>
             <li>${place.city}, ${place.country}
               <ul>
@@ -26,16 +29,67 @@ $(document).ready(function(){
               </ul>
             </li>
           </ul>
+          </div>
         `);
-
-      })
+      });
     }
-
-    var onError = function(res){
+    function onErrorPlaces(res){
       console.log("ERROR");
     }
+  });
 
-  // $('.places_wrapper').append
+  // display my profile info
+  $("#profile").click(function(){
+    $("div.places").remove();
+    $.ajax({
+      method: 'GET',
+      url: '/api/profile',
+      data: 'json',
+      success: onSuccessProfile,
+      error: onErrorProfile
+    })
+    function onSuccessProfile(profileObject){
+      $(".profile-wrapper").append(`
+        <div class="profile col-md-6">
+          <h3>${profileObject.name}</h3>
+          <img class="img-responsive" src="${profileObject.githubProfileImage}">
+        </div>
+          <div class="col-md-6">
+            <ul>
+              <li>${profileObject.pets[0].name} my current ${profileObject.pets[0].type}</li>
+              <li>${profileObject.pets[1].name} my parents ${profileObject.pets[1].type}</li>
+              <li>${profileObject.pets[2].name} my parents ${profileObject.pets[3].breed} ${profileObject.pets[2].type}</li>
+              <li>${profileObject.pets[3].name} my parents ${profileObject.pets[3].breed} ${profileObject.pets[3].type}</li>
+            </ul>
+          </div>
+        `)
+    }
 
+    function onErrorProfile(res){
+      console.log("ERROR");
+    }
+  })
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
